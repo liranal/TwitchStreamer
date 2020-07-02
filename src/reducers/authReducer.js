a67b2bedf6b7f9d9c66adca9dd584bb55f7a4317
liranal <liraneilary@gmail.com>
2020-07-02 16:58:12 +0300

    Add types for Actions

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,13 +1,15 @@
+import { SIGN_OUT, SIGN_IN } from "../actions/types";
+
 const INTIAL_STATE = {
   isSignIn: null,
 };
 
 export default (state = INTIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: true };
 
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: false };
 
     default:
